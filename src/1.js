d625e16193da18d8ae5ed0f90f37a3a93589a290
scherlucky111 <scherlucky111@gmail.com>
2025-03-06 15:51:11 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,7 @@
+// Random JS Code
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let randNum = getRandomInt(10); // Generate a random number between 0 and 9
+console.log("Generated Random Number:", randNum);
