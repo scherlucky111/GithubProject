6d7ba9d61fdc6e957624e7d790c9d4bca0d3f9f8
scherlucky111 <scherlucky111@gmail.com>
2025-03-16 03:40:04 +0000

    Add 10.js

---
+++ src/10.js
@@ -0,0 +1,6 @@
+// JavaScript program to demonstrate how to use the Math.random() function 
+
+console.log("Math.random() Example"); 
+
+let randomNumber = Math.random(); 
+ console.log(randomNumber); 
