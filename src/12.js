bfc72980aa43c6efe97d8088b978dad0457ec11d
scherlucky111 <scherlucky111@gmail.com>
2025-03-18 10:05:07 +0000

    Add 12.js

---
+++ src/12.js
@@ -0,0 +1,7 @@
+let randomNumber = Math.floor(Math.random() * 10);
+console.log(`The number is ${randomNumber}`);
+
+// OR
+
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(`The number is ${randomNumber}`);
